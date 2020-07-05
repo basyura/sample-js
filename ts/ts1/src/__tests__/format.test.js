bf156d04c2edbf9caaf9c0963715f29258482623
basyura <basyura@gmail.com>
2020-07-05 15:17:11 +0900

    add test

---
+++ ts/ts1/src/__tests__/format.test.js
@@ -0,0 +1,9 @@
+const { format } = require("../format");
+
+describe("format", () => {
+  test("only root", () => {
+    expect(
+      format({ type: "directory", name: "root", children: [] })
+    ).toMatchSnapshot();
+  });
+});
