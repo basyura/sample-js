049b61e6b4552a7befa66b3fbc93a311155b3427
basyura <basyura@gmail.com>
2021-01-25 23:08:59 +0900

    add todo

---
+++ vue/vue-todo-list/src/components/ToDoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="todo_container">
+    <ul>
+      <li v-bind:title="todo">
+        {{ todo.slice(0, 10) + "..." }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return { todo: "Go out to sea" };
+  },
+};
+</script>
+
+<style>
+.todo_container {
+  text-align: left;
+}
+</style>
