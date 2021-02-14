5251cd976fa4488a38e196de03f7339d177d04c4
basyura <basyura@gmail.com>
2021-02-14 17:12:31 +0900

    use slot

--- vue/vue-todo-list/src/components/ToDoItem.vue
+++ vue/vue-todo-list/src/components/ToDoItem.vue
@@ -1,16 +1,17 @@
 <template>
   <li class="todo-item" v-bind:class="{ done: done }" v-on:click="handleClick">
     <span v-if="done">✔</span>
-    {{ text }}
+    <!-- {{ text }} -->
+    <slot />
   </li>
 </template>
 
 <script>
 export default {
   props: {
-    text: {
-      type: String,
-    },
+    // text: {
+    //   type: String,
+    // },
     done: {
       type: Boolean,
     },
